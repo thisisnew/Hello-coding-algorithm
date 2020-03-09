a002e38e4675ba5439fc176f231ecad463fa3006
thisisnew <thisisnew@naver.com>
2020-03-09 22:03:39 +0900

    선택정렬. 다른 책 참고.

---
+++ hello-coding-algorithm/src/Ch02/SelectionSort.java
@@ -0,0 +1,33 @@
+package Ch02;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int arr[] = {4,5,6,1,3,2};
+		 
+        sort(arr);
+ 
+        for(int i=0; i<arr.length; i++){
+            System.out.println("arr["+i+"] : " + arr[i]);
+        }
+
+	}
+	
+	public static void sort(int[] arr){
+        int size = arr.length;
+        int min;
+        int temp;
+        
+        for(int i=0; i<size-1; i++){ // size-1 : 마지막 요소는 자연스럽게 정렬됨
+            min = i;
+            for(int j=i+1; j<size; j++){
+                if(arr[min] > arr[j]){
+                    min = j;
+                }
+            }
+            temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
