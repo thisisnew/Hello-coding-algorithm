3fc3f138aaf3326c3e398d04eb8c3e35a99f23f8
thisisnew <thisisnew@naver.com>
2020-03-09 22:04:30 +0900

    선택정렬 공부

--- hello-coding-algorithm/src/Ch02/SelectionSort.java
+++ hello-coding-algorithm/src/Ch02/SelectionSort.java
@@ -18,7 +18,7 @@ public class SelectionSort {
         int min;
         int temp;
         
-        for(int i=0; i<size-1; i++){ // size-1 : 마지막 요소는 자연스럽게 정렬됨
+        for(int i=0; i<size-1; i++){
             min = i;
             for(int j=i+1; j<size; j++){
                 if(arr[min] > arr[j]){
